63c9de0ad061997b900f8f9cd97697758937d65f
zzxming <zzxming77@163.com>
2024-01-07 23:23:39 +0800

    refactor: 新增extend

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -3,7 +3,16 @@ import type { Config } from 'tailwindcss';
 const config: Config = {
     content: ['./src/**/*.{ts,tsx}'],
     theme: {
-        extend: {},
+        extend: {
+            colors: {
+                success: '#52c41a',
+                warn: '#faad14',
+                danger: '#ff4d4f',
+            },
+            gridTemplateColumns: {
+                command_list: '100px minmax(0, 1fr)',
+            },
+        },
     },
     plugins: [],
 };
