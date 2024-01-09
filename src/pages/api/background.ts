ac0b5422706b43738161bef2915e9536746aafca
zzxming <zzxming77@163.com>
2024-01-09 21:06:25 +0800

    fix: 请求方式限制

--- src/pages/api/background.ts
+++ src/pages/api/background.ts
@@ -4,6 +4,10 @@ import axios from 'axios';
 const types = ['meizi', 'dongman', 'fengjing', 'suiji'];
 
 export default async (req: NextApiRequest, res: NextApiResponse) => {
+    if (req.method !== 'GET') {
+        res.status(405).send({ message: 'Only GET requests allowed' });
+        return;
+    }
     try {
         let { type } = req.query;
         if (type instanceof Array) {
