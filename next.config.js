ed6759211680904c59789b225a0af3df1b11e1be
zzxming <zzxming77@163.com>
2024-01-14 21:41:25 +0800

    feat: ErrorBoundary和svg模块

--- next.config.js
+++ next.config.js
@@ -1,5 +1,13 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     reactStrictMode: true,
+    webpack: (config, options) => {
+        config.module.rules.push({
+            test: /\.svg$/,
+            use: ['@svgr/webpack'],
+        });
+
+        return config;
+    },
 };
 module.exports = nextConfig;
